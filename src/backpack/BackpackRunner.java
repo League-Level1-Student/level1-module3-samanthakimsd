40451a90f3bea1ba0019d6a7c14dd37be50b9e54
League <league@WTS-IM-17.attlocal.net>
2019-05-05 17:27:12 -0700

    i finished backpack and flappy bird

--- src/backpack/BackpackRunner.java
+++ src/backpack/BackpackRunner.java
@@ -3,6 +3,16 @@ package backpack;
 public class BackpackRunner {
 	public static void main(String[] args) {
 		new Backpack().packAndCheck();
-
+ Ruler r = new Ruler();
+ Pencil p = new Pencil(); 
+ Textbook t = new Textbook();
+ Backpack b = new Backpack();
+ b.putInBackpack(p);
+p.write("Matt is weird lol");
+ r.measure();
+ t.read();
+b.putInBackpack(r);
+b.putInBackpack(t);
+b.packAndCheck();
 	}
 }
