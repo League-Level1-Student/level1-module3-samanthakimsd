c6bae860a4dc4bcd2e6574b4722b9b50ed77d93e
League Student <league@iMac.attlocal.net>
2019-06-09 17:13:18 -0700

    i finisehd netflix

--- src/netflix/Netflix.java
+++ src/netflix/Netflix.java
@@ -5,6 +5,21 @@ public static void main(String[] args) {
 	
 	Movie m = new Movie("Avengers Endgame", 5);
 	Movie n = new Movie("Spiderman Far from Home",500000);
+	Movie o = new Movie("Captain Marvel",5);
+	Movie p = new Movie("Bee Movie",0);
+	Movie q = new Movie("Emoji Movie",0);
 	
+	
+	System.out.println(m.getTicketPrice());
+	NetflixQueue matt = new NetflixQueue();
+	matt.addMovie(o);
+	matt.addMovie(m);
+	matt.addMovie(q);
+	matt.addMovie(p);
+	matt.addMovie(n);
+	matt.printMovies();
+	
+	System.out.println("the best movie is " +matt.getBestMovie());
+	System.out.println("the best movie is " +matt.getSecondBestMovie());
 }
 }
